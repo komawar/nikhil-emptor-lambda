3f01ab1a23f20b60f286eb7e3fcd7c516f502e44
Nikhil Komawar <nik.komawar@gmail.com>
2019-06-02 17:49:35 +0530

    Add ability store in s3 bucket and DynamoDB table
    This commit includes the following changes and assumptions:
      * This commit keeps the initial functionality of getting url as
      argument, making a request and extracting the title from it.
    
      * A ```store_to_s3``` method within the lambda function, facilitates
      creation of bucket named "patronemptor-version2" using the boto's
      create_bucket call. This call being idempotent, even if bucket exists
      it should succeed with noop.
    
      * The bucket name has been kept constant for the purposes of this
      code.
    
      * An obj with ramdomly generated uuid stores the response body in this
      bucket.
    
      * Object URL is generated using the AWS standard specifications and is
      returned to the call as a part of the json return response.
    
      * It also adds a ```store_to_dynamodb``` method within the lambda
      function. This acts as facilitator for creation of a DynamoDB table
      named "version2" with a single attribute and key named "title". If
      table already exists it catches the "ResourceInUseException" and
      prints it, so that it can be logged to CloudWatch logs.
    
      * A table entry is created to store the "title".
    
       * To invoke this lambda function command
       ```serverless invoke -f weblambda -l -d "http://example.com" --raw```
       has to be used.
    
    In this implementation there are a few assumptions:
    
      * The assumptions from first version of the project are valid.
    
      * The bucket and table names are hardcoded in the code and their
      creation is done by the lambda function itself.
    
      * A serverless template to create these resources was made however due
      to permission issues, that approach was not followed.

--- patronemptor/handler.py
+++ patronemptor/handler.py
@@ -1,6 +1,71 @@
 import re
+import uuid
 
+import boto3
 from botocore.vendored import requests
+
+
+s3_client = boto3.client('s3', region_name='us-east-1')
+dynamodb_client = boto3.client('dynamodb', region_name='us-east-1')
+dynamodb = boto3.resource('dynamodb')
+
+table_name = 'version2'
+
+
+def create_table():
+    try:
+        return dynamodb_client.create_table(
+            AttributeDefinitions=[
+                {
+                    'AttributeName': 'title',
+                    'AttributeType': 'S'
+                },
+            ],
+            TableName=table_name,
+            KeySchema=[
+                {
+                    'AttributeName': 'title',
+                    'KeyType': 'HASH'
+                }
+            ],
+            ProvisionedThroughput={
+                'ReadCapacityUnits': 5,
+                'WriteCapacityUnits': 5
+            },
+        )
+    except dynamodb_client.exceptions.ResourceInUseException as e:
+        print ("Skipping table creation, already exists" + str(e))
+        return None
+
+
+def extract_title(r):
+    return re.search(
+        '(?<=<title>).+?(?=</title>)',
+        r.text,
+        re.DOTALL
+    ).group().strip()
+
+
+def store_to_s3(r):
+    bucket = 'patronemptor-version2'
+    s3_client.create_bucket(Bucket=bucket)
+    obj = str(uuid.uuid4())
+
+    s3_client.put_object(Body=r.text, Bucket=bucket, Key=obj)
+    obj_url = 'https://%s.s3.amazonaws.com/%s' % (bucket, obj)
+
+    return obj_url
+
+
+def store_to_dynamodb(title):
+    create_table()
+    table = dynamodb.Table(table_name)
+
+    table.put_item(
+        Item={
+            'title': title
+        }
+    )
 
 
 def url_parser(event, context):
@@ -8,10 +73,13 @@ def url_parser(event, context):
 
     r = requests.get(url)
 
-    title = re.search(
-        '(?<=<title>).+?(?=</title>)',
-        r.text,
-        re.DOTALL
-    ).group().strip()
+    title = extract_title(r)
 
-    return title
+    obj_url = store_to_s3(r)
+
+    store_to_dynamodb(title)
+
+    return {
+        "title": title,
+        "S3-URL": obj_url
+    }
